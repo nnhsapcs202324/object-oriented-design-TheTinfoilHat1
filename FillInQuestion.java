dfdc2e33de36320231770997123eb2d285c243dc
Aiden Xie <amxie@stu.naperville203.org>
2024-02-07 09:38:33 -0600

    Completed the first segment of live coding

---
+++ FillInQuestion.java
@@ -0,0 +1,16 @@
+
+/**
+ * A FillInQuestion is constructed with a string that contains the answer surrounded '_'. For example,
+ * "The inventor of Java was _James Gosling_."
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class FillInQuestion extends Question
+{
+    @Override
+    public void setText(String questionText)
+    {
+        
+    }
+}
